9b53551cf23c44d52b09b368c5829bb8fd0c17e2
atelierhs <changhs1227@gmail.com>
2026-04-02 01:18:18 +0900

    fead: hobby section

--- Week2/JHS/src/App.jsx
+++ Week2/JHS/src/App.jsx
@@ -1,6 +1,7 @@
 import Header from './components/Header'
 import Hero   from './components/Hero'
 import About  from './components/About'
+import Hobby  from './components/Hobby'
 
 const PROFILE = {
   name:  '장현서',
@@ -16,6 +17,12 @@ const PROFILE = {
   activities: ['멋쟁이 사자처럼 14기'],
 }
 
+const HOBBIES = [
+  { label: '넷플릭스 보기', img: '/images/netflix.png', alt: '취미1' },
+  { label: '잠자기',        img: '/images/sleep.png',   alt: '취미2' },
+  { label: '맛집 탐방',     img: '/images/store.png',   alt: '취미3' },
+]
+
 export default function App() {
   return (
     <>
@@ -24,7 +31,8 @@ export default function App() {
         name="장현서"
         message="멋있는 개발자가 되고 싶어요!"
       />
-      <About profile={PROFILE} />
+      <About  profile={PROFILE} />
+      <Hobby  hobbies={HOBBIES} />
       <div id="contact" />
     </>
   )
--- Week2/JHS/src/components/Hobby.jsx
+++ Week2/JHS/src/components/Hobby.jsx
@@ -1,27 +1,21 @@
 import styles from '../styles/Hobby.module.scss'
 
-export default function Hobby() {
+export default function Hobby({ hobbies }) {
   return (
     <section className={styles.hobbySection} id="hobby">
       <h2 className={styles.sectionTitle}>HOBBY</h2>
 
       <div className={styles.hobbyGrid}>
-
-        <div className={styles.hobbyCard}>
-          <div className={styles.hobbyHeader}><span>넷플릭스 보기</span></div>
-          <div className={styles.hobbyImgWrap} />
-        </div>
-
-        <div className={styles.hobbyCard}>
-          <div className={styles.hobbyHeader}><span>잠자기</span></div>
-          <div className={styles.hobbyImgWrap} />
-        </div>
-
-        <div className={styles.hobbyCard}>
-          <div className={styles.hobbyHeader}><span>맛집 탐방</span></div>
-          <div className={styles.hobbyImgWrap} />
-        </div>
-
+        {hobbies.map(({ label, img, alt }) => (
+          <div className={styles.hobbyCard} key={label}>
+            <div className={styles.hobbyHeader}>
+              <span>{label}</span>
+            </div>
+            <div className={styles.hobbyImgWrap}>
+              <img src={img} alt={alt} className={styles.hobbyImg} />
+            </div>
+          </div>
+        ))}
       </div>
     </section>
   )
